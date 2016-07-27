5f419c5a8666e215b9ba64ee02ee4261a6661008
jcherqui <joachim.cherqui@viacesi.fr>
2016-07-27 10:05:31 +0200

    Add tests

--- lib/computer.go
+++ lib/computer.go
@@ -1,18 +1,16 @@
 package computer
 
-import "fmt"
-
 const maxTick = 5
 const width, height = 5, 5
 const up, right, down, left = 0, 1, 2, 3
 const block = 1;
 const apple = 2;
 
-func moveSnake(snake [][2]int, path []int) [][2]int {
+func MoveSnake(snake [][2]int, moves []int) [][2]int {
 
-    for move := range path {
+    for _, move := range moves {
         snakeHead := GetSnakeHead(snake)
-        nextPosition := getAdjacentPosition(snakeHead, move)
+        nextPosition := GetAdjacentPosition(snakeHead, move)
 
         snake = snake[1:]
         snake = append(snake, nextPosition)
@@ -21,7 +19,7 @@ func moveSnake(snake [][2]int, path []int) [][2]int {
     return snake
 }
 
-func getAdjacentPosition(position [2]int, move int) [2]int {
+func GetAdjacentPosition(position [2]int, move int) [2]int {
     var x = position[0] // @FIXME: use destructuring
     var y = position[1]
 
@@ -58,7 +56,7 @@ func IsOutsideBoundingBox(position [2]int, grid [width][height]int) bool {
 }
 
 func getMoveScore(move int, snake [][2]int, apple [2]int) int {
-    newSnake := moveSnake(snake, []int{move})
+    newSnake := MoveSnake(snake, []int{move})
 
     if isSnakeHeadAtPosition(newSnake, apple) {
         return 10
@@ -78,12 +76,12 @@ func IsEmptyCell(grid [width][height]int, position [2]int) bool {
     return false
 }
 
-func getPossibleMoves(grid [width][height]int, snake [][2]int) []int {
+func GetPossibleMoves(grid [width][height]int, snake [][2]int) []int {
     var head = GetSnakeHead(snake)
 
     var possibleMoves []int
     for _, move := range []int{up, right, down, left} {
-        var adjacentPosition = getAdjacentPosition(head, move)
+        var adjacentPosition = GetAdjacentPosition(head, move)
 
         if !IsOutsideBoundingBox(adjacentPosition, grid) && IsEmptyCell(grid, adjacentPosition) {
             possibleMoves = append(possibleMoves, move)
@@ -105,7 +103,7 @@ func GetPath(grid [width][height]int, snake [][2]int, apple [2]int) []int {
     var paths [][]int
     var scores []int
 
-    var possibleMoves = getPossibleMoves(grid, snake)
+    var possibleMoves = GetPossibleMoves(grid, snake)
 
     for _, possibleMove := range possibleMoves {
         scores = append(scores, getMoveScore(possibleMove, snake, apple))
@@ -114,10 +112,9 @@ func GetPath(grid [width][height]int, snake [][2]int, apple [2]int) []int {
 
     for tick := 1; tick < maxTick; tick++ {
         for _, path := range paths {
-            newSnake := moveSnake(snake, path)
-            fmt.Println(newSnake)
+            newSnake := MoveSnake(snake, path)
 
-            for _, possibleMove := range getPossibleMoves(grid, newSnake) {
+            for _, possibleMove := range GetPossibleMoves(grid, newSnake) {
                 newPath := append(path, possibleMove)
                 scores = append(scores, getMoveScore(possibleMove, newSnake, apple))
                 paths = append(paths, newPath)
--- lib/computer_test.go
+++ lib/computer_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 const width, height = 5, 5
+const up, right, down, left = 0, 1, 2, 3
 
 func TestComputer(t *testing.T) {
     RegisterFailHandler(Fail)
@@ -37,7 +38,7 @@ var _ = Describe("Computer", func() {
         Expect(IsEmptyCell(grid, [2]int{2, 2})).To(Equal(true))
     })
 
-    It("it should check outside bounding box", func() {
+    It("should check outside bounding box", func() {
         grid := [width][height]int{
             {1, 1, 1, 0, 0},
             {0, 0, 0, 0, 0},
@@ -48,5 +49,70 @@ var _ = Describe("Computer", func() {
 
         Expect(IsOutsideBoundingBox([2]int{-1, 0}, grid)).To(Equal(true))
         Expect(IsOutsideBoundingBox([2]int{0, 0}, grid)).To(Equal(false))
+    })
+
+    It("should return adjacent position", func() {
+        Expect(GetAdjacentPosition([2]int{1, 0}, up)).To(Equal([2]int{0, 0}))
+        Expect(GetAdjacentPosition([2]int{0, 0}, right)).To(Equal([2]int{0, 1}))
+        Expect(GetAdjacentPosition([2]int{0, 0}, down)).To(Equal([2]int{1, 0}))
+        Expect(GetAdjacentPosition([2]int{0, 1}, left)).To(Equal([2]int{0, 0}))
+    })
+
+    It("should return possible moves", func() {
+        grid := [width][height]int{
+            {1, 1, 1, 0, 0},
+            {0, 0, 1, 0, 0},
+            {2, 1, 1, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+        }
+
+        snake := [][2]int{
+            {0, 0},
+            {0, 1},
+            {0, 2},
+            {1, 2},
+            {2, 2},
+            {2, 1},
+        }
+
+        Expect(GetPossibleMoves(grid, snake)).To(Equal([]int{0, 2, 3}))
+    })
+
+    It("should move snake", func() {
+        snake := [][2]int{
+            {0, 0},
+            {0, 1},
+            {0, 2},
+        }
+
+        newSnake := MoveSnake(snake, []int{right, right, down})
+
+        Expect(newSnake).To(Equal([][2]int{
+            {0, 3},
+            {0, 4},
+            {1, 4},
+        }))
+    })
+
+    It("should find path", func() {
+        grid := [width][height]int{
+            {1, 1, 1, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 2, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+        }
+
+        snake := [][2]int{
+            {0, 0},
+            {0, 1},
+            {0, 2},
+        }
+
+        apple := [2]int{2, 2}
+
+        path := GetPath(grid, snake, apple)
+        Expect(path).To(Equal([]int{down, down}))
     })
 })
